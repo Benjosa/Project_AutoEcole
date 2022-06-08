08553bac2f8c175958e52d0878a05f18be67af7d
jonathansablier <jonathansablier@localhost>
2022-06-08 17:17:13 +0200

    Creation du projet auto-ecole sans l'application properties

---
+++ src/main/java/com/inti/Project_AutoEcole/ProjectAutoEcoleApplication.java
@@ -0,0 +1,13 @@
+package com.inti.Project_AutoEcole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAutoEcoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAutoEcoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inti/Project_AutoEcole/ProjectAutoEcoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti.Project_AutoEcole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAutoEcoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
